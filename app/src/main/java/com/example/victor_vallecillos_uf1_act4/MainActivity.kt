570bb24792cade5cd0b800282882af06e5924797
VictorVaCa <a17vicvalcam@inspedralbes.cat>
2024-10-18 16:44:38 +0200

    Pràctica finalitzada

--- app/src/main/java/com/example/victor_vallecillos_uf1_act4/MainActivity.kt
+++ app/src/main/java/com/example/victor_vallecillos_uf1_act4/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.victor_vallecillos_uf1_act4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonLogcat = findViewById<Button>(R.id.buttonLogcat);
+
+        buttonLogcat.setOnClickListener{
+            Log.d("Missatge","Botó apretat");
+        }
+
+
+        val buttonToast = findViewById<Button>(R.id.buttonToast);
+
+        buttonToast.setOnClickListener{
+            Toast.makeText(this, "Botó apretat", Toast.LENGTH_SHORT).show();
+        }
     }
 }
